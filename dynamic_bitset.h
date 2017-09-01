eb49a7ab915459c3717e1f5ba2135c44867fa01a
tigertang <491506913@qq.com>
2017-09-01 15:03:00 +0800

    fix some bugs

--- dynamic_bitset.h
+++ dynamic_bitset.h
@@ -7,7 +7,6 @@
 
 #include <bitset>
 #include <vector>
-
 class dynamic_bitset
 {
     static const size_t unit_size = 64;
@@ -18,13 +17,13 @@ class dynamic_bitset
     typedef const unit &const_unit_reference;
     typedef unit::reference reference;
 
-private:
+  private:
     size_type _size;
     std::vector<unit> _v;
 
     inline void _push(size_type, const_unit_reference, size_type, difference_type);
 
-public:
+  public:
     dynamic_bitset();
 
     explicit dynamic_bitset(size_type);
@@ -41,13 +40,13 @@ public:
 
     void resize(size_type);
 
-    void reset();
+    dynamic_bitset &reset();
 
-    void set();
+    dynamic_bitset &set();
 
     dynamic_bitset operator^(const dynamic_bitset &) const;
 
-    void operator^=(const dynamic_bitset &);
+    dynamic_bitset &operator^=(const dynamic_bitset &);
 
     bool test(size_type) const;
 };
--- main.cpp
+++ main.cpp
@@ -1,51 +1,59 @@
 #include <bits/stdc++.h>
 #include "dynamic_bitset.h"
 
+// ===============
+// dynamic_bitset
+//  library demo
+//   hdu 6085
+// ===============
+
 using namespace std;
 const int N = 50010;
 int n, m, q, a[N], b[N];
 
 dynamic_bitset A, ans;
 
-void pri(const string &s) {
-    cout << s << endl;
-    fflush(stdout);
-}
-
-void deal(int b) {
+void deal(int b)
+{
     static dynamic_bitset tmp, _tmp;
     tmp = dynamic_bitset(b);
-    for (int i = 0; i * b <= a[n]; i++) {
+    for (int i = 0; i * b <= a[n]; i++)
+    {
         _tmp = dynamic_bitset(A, i * b, min(b, a[n] - i * b + 1));
         tmp ^= _tmp;
     }
     ans ^= tmp;
-    cout << ans.to_string() << endl;
 }
 
-int main() {
+int main()
+{
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         cin >> n >> m >> q;
         for (int i = 1; i <= n; i++)
             scanf("%d", a + i);
+        for (int i = 1; i <= m; i++)
+            scanf("%d", b + i);
         sort(a + 1, a + n + 1);
+        sort(b + 1, b + m + 1);
+
         A.resize(a[n] + 1);
         A.reset();
-        ans.resize(a[n] + 1);
+        ans.resize(b[m]);
         ans.reset();
 
         for (int i = 1; i <= n; i++)
             A[a[i]] = 1;
         for (int i = 1; i <= m; i++)
-            scanf("%d", b + i);
-        for (int i = 1; i <= m; i++)
             deal(b[i]);
-        while (q--) {
+        while (q--)
+        {
             int x;
             scanf("%d", &x);
             printf("%d\n", ans.test(x));
         }
     }
+    return 0;
 }
